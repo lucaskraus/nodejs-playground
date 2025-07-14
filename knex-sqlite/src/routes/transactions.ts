317fcac736c2342cda70c8a5fb65fdff03e1b380
lucaskraus <lucas-kraus@hotmail.com>
2025-07-14 20:34:54 -0300

    Update session cookie maxAge in transaction routes for improved session management

--- knex-sqlite/src/routes/transactions.ts
+++ knex-sqlite/src/routes/transactions.ts
@@ -45,7 +45,7 @@ export async function transactionRoutes(app: FastifyInstance) {
       sessionId = randomUUID();
       res.cookie('sessionId', sessionId, {
         path: '/',
-        maxAge: 1000 * 60 * 60 * 24 * 30,
+        maxAge: 60 * 60 * 24 * 30,
       });
     }
 
