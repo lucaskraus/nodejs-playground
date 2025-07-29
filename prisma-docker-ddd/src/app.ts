636baebf3645651c690ed7e15b6b4acbd306a7fd
lucaskraus <lucas-kraus@hotmail.com>
2025-07-29 20:41:59 -0300

    Add internal server error handling in Fastify app
    - Enhanced the global error handler in app.ts to return a 500 status code with a structured message for internal server errors, improving overall error management and client feedback.

--- prisma-docker-ddd/src/app.ts
+++ prisma-docker-ddd/src/app.ts
@@ -12,6 +12,8 @@ app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
     return reply.status(400).send({ message: 'Validation error', issues: error.issues })
   }
+
+  return reply.status(500).send({ message: 'Internal server error' })
 })
 
 app.listen({ port: env.PORT, host: "0.0.0.0" }, (err, address) => {
