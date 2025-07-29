33aa48fb77c8b493e1624262681b8154302e3cd3
lucaskraus <lucas-kraus@hotmail.com>
2025-07-29 20:41:14 -0300

    Add custom error handling for Zod validation in Fastify app
    - Implemented a global error handler in app.ts to catch Zod validation errors and return a structured response with a 400 status code, improving error feedback for clients.

--- prisma-docker-ddd/src/app.ts
+++ prisma-docker-ddd/src/app.ts
@@ -2,10 +2,17 @@
 import { env } from "./env";
 import fastify from "fastify";
 import { appRoutes } from "./http/routes";
+import { ZodError } from "zod";
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error', issues: error.issues })
+  }
+})
 
 app.listen({ port: env.PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
